e7939fce5776f0012600fe97e8d2e7119d7bb037
vangradt <amvangradt@gmail.com>
2018-10-25 15:41:32 +0100

    probably brute

--- questions/question1.js
+++ questions/question1.js
@@ -1,7 +1,20 @@
 // Use this file to answer question 1
 
 function answer01(portfolios) {
-    return -1
+
+    let max = 0;
+
+    for (let i = 0; i < portfolios.length - 1; i++) {
+        for (let j = i + 1; j < portfolios.length; j++) {
+            let sum = portfolios[i] ^ portfolios[j];
+
+            if (sum > max) {
+                max = sum;
+            }
+        }
+    }
+
+    return max;
 }
 
 module.exports = answer01
