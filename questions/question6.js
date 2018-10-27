46c7704a0b1ac3bbfd9234d35debe1743f2273a1
Adam <amvangradt@gmail.com>
2018-10-27 18:03:57 +0100

    q6 first attempt

--- questions/question6.js
+++ questions/question6.js
@@ -1,7 +1,29 @@
 // Use this file to answer question 6
 
 function answer06(numServers, targetServer, times) {
-    return -1
+
+    let d = [];
+
+    for (let i = 0; i < numServers; i++) {
+        d.push(Number.MAX_SAFE_INTEGER);
+    }
+
+    d[0] = 0;
+
+    let q = [0];
+
+    while (q.length > 0) {
+        let current = q.shift();
+
+        for(let i = 0; i < numServers; i++) {
+            if (d[i] > d[current] + times[current][i]) {
+                q.push(i);
+                d[i] = d[current] + times[current][i];
+            }
+        }
+    }
+
+    return d[targetServer];
 }
 
 module.exports = answer06
