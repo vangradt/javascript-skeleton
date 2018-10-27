655b6fcc44d54672652931dbfcf981d21e04028d
Adam <amvangradt@gmail.com>
2018-10-27 18:57:29 +0100

    q5 first attempt

--- questions/question5.js
+++ questions/question5.js
@@ -1,7 +1,27 @@
 // Use this file to answer question 5
 
 function answer05(allowedAllocations, totalValue) {
-    return -1
+    let T = [0];
+
+    for(let i = 1; i <= totalValue; i++) {
+        T.push(Number.MAX_SAFE_INTEGER);
+    }
+
+    allowedAllocations.sort(function(a, b){return a-b});
+
+    for (let j = 0; j < allowedAllocations.length; j++) {
+        let current = allowedAllocations[j];
+
+        for (let i = 0; i <= totalValue - current; i++) {
+            if (T[i] < Number.MAX_SAFE_INTEGER) {
+                if (T[i] + 1 < T[i + current]) {
+                    T[i + current] = T[i] + 1;
+                }
+            }
+        }
+    }
+
+    return T[totalValue];
 }
 
 module.exports = answer05
