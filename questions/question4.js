ea886b6f3917c88f12e8944c1eea092ec696377b
Adam <amvangradt@gmail.com>
2018-10-28 13:40:29 +0000

    q4 first attempt

--- questions/question4.js
+++ questions/question4.js
@@ -1,7 +1,40 @@
 // Use this file to answer question 4
 
 function answer04(rows, numberMachines) {
-    return -1
+    let min = Number.MAX_SAFE_INTEGER;
+
+    for (let i = 0; i < rows.length; i++) {
+        let l = 0;
+
+        let sum = 0;
+
+        for(let j = 0; j < rows[i].length; j++) {
+            if(rows[i][j] === 'X') {
+                l = 0;
+                sum = 0;
+            }
+            else {
+                l++;
+
+                sum += rows[i][j];
+
+                if(l === numberMachines && sum < min) {
+                    min = sum;
+
+                    sum -= rows[i][j - l + 1];
+
+                    l--;
+                }
+
+
+            }
+        }
+    }
+
+    if(min !== Number.MAX_SAFE_INTEGER)
+        return min;
+    else
+        return 0
 }
 
 module.exports = answer04
