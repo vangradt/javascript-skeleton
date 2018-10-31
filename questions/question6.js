7ca446072c6a4301b0e39a1ebb596dd5ac2a77ba
Adam <amvangradt@gmail.com>
2018-10-31 18:59:34 +0000

    get back to solutions

--- questions/question6.js
+++ questions/question6.js
@@ -1,10 +1,38 @@
 // Use this file to answer question 6
 
-
+let PriorityQueue =  require('js-priority-queue');
 
 function answer06(numServers, targetServer, times) {
 
-    return -1;
+    let d = [];
+
+    for (let i = 0; i < numServers; i++) {
+        d.push(Number.MAX_SAFE_INTEGER);
+    }
+
+    d[0] = 0;
+
+    let compare = function(a, b) {
+        return a.d - b.d;
+    }
+
+    let q = new PriorityQueue({comparator: compare});
+
+    q.queue({n: 0, d: d[0]});
+
+    while (q.length > 0) {
+        let current = q.dequeue();
+
+        for(let i = 0; i < numServers; i++) {
+            if (d[i] > d[current.n] + times[current.n][i]) {
+                d[i] = d[current.n] + times[current.n][i];
+
+                q.queue({n: i, d: d[i]});
+            }
+        }
+    }
+
+    return d[targetServer];
 }
 
 module.exports = answer06
